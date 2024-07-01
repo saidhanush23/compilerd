750556128ed6f767347bf252604cf6adcbe539ab
saidhanush23 <137997876+saidhanush23@users.noreply.github.com>
2024-07-01 19:36:33 +0530

    Update router.js

--- router.js
+++ router.js
@@ -1,8 +1,9 @@
-const express = require('express')
-const router = express.Router()
+// routes/index.js
+const express = require('express');
+const router = express.Router();
 
-const codeRouter = require('./routers/code.router')
+const codeRouter = require('./routers/code.router');
 
-router.use('/', codeRouter)
+router.use('/api', codeRouter); // Assuming codeRouter handles routes starting with '/api'
 
-module.exports = router
+module.exports = router;
